35576131a635dc3db50b702602cb52066d676aa3
者霖 陈 <2824964418@qq.com>
2023-11-29 20:39:52 +0800

    Add UserInfo model and new apps

--- user/models.py
+++ user/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 # Create your models here.
-class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    userid = models.CharField(max_length=20)
-    nickName = models.CharField(max_length=100)
+class UserInfo(models.Model):
     avatarUrl = models.CharField(max_length=200)
+    nickName = models.CharField(max_length=20)
+    signature = models.CharField(max_length=100)
+    
+    
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True) # an int automatically incremented
+    userid = models.CharField(max_length=20) # the openid from wechat
+    nickName = models.CharField(max_length=100)
+    userInfo = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+    
