94a8c0738c0edecc74029cdde4a199394a41b839
LC <lichang21@mails.tsinghua.edu.cn>
2023-12-02 23:18:12 +0800

    会议记录

--- backend/Healthu/user/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    nickName = models.CharField(max_length=100)
-    avatarUrl = models.CharField(max_length=200)
