e71ee9961cd3b8d230be59ca28f9747df78b94ce
者霖 陈 <2824964418@qq.com>
2023-12-05 14:57:48 +0800

    add addact

--- schedule/views.py
+++ schedule/views.py
@@ -25,22 +25,29 @@ def todos(request):
         # else: not found
         return HttpResponse("Schedule not found", status=400)
            
-def deleteTodo():
+def deleteTodo(request):
     if request.method == 'GET':
         id = request.GET.get("id")
         oldTodo = request.GET.get("oldTodo")
         # find the schedule (if any) according to the id
         targetSchedule = Schedule.objects.filter(id=id).first()
         if targetSchedule:
-            # find in Schedule.todos by the date
+            # find in Schedule.todos by the date, title, start and end
             allTodos = targetSchedule.todos # JSONField 
+            todoFound = False
             for todo in allTodos:
                 if todo['date'] == oldTodo['date']\
                 and todo['title'] == oldTodo['title']\
                 and todo['start'] == oldTodo['start']\
                 and todo['end'] == oldTodo['end']:
+                    
+                    todoFound = True
+                    
                     todo.delete() # delte all todos with the same date, title, start and end
-            return HttpResponse("Delete successfully")
+            if todoFound:
+                return HttpResponse("Delete successfully")
+            # nothitng to delete
+            return HttpResponse("Todo not found", status=400)
         # else: not found
         return HttpResponse("Schedule not found", status=400)
                   
@@ -53,13 +60,17 @@ def changeTodo(request):
         # find the schedule (if any) according to the id
         targetSchedule = Schedule.objects.filter(id=id).first()
         if targetSchedule:
-            # find in Schedule.todos by the date
+            # find in Schedule.todos by the date, title, start and end
             allTodos = targetSchedule.todos
+            todoFound = False
             for todo in allTodos:
                 if todo['date'] == oldTodo['date']\
                 and todo['title'] == oldTodo['title']\
                 and todo['start'] == oldTodo['start']\
                 and todo['end'] == oldTodo['end']:
+                    
+                    todoFound = True
+                    
                     todo['date'] = newTodo['date']
                     todo['title'] = newTodo['title']
                     todo['start'] = newTodo['start']
@@ -69,12 +80,21 @@ def changeTodo(request):
                     todo['state'] = newTodo['state']
                     todo['sportType'] = newTodo['sportType']
                     todo['sportState'] = newTodo['sportState']
-            return HttpResponse("Change successfully")
+            if todoFound:
+                return HttpResponse("Change successfully")
+            # nothing to change
+            return HttpResponse("Todo not found", status=400)
         # else: not found
         return HttpResponse("Schedule not found", status=400)
 
 def addAct(request):
-    return HttpResponse("Hello, world. You're at the schedule addAct.")
+    if request.method == 'GET':
+        id = request.GET.get("id")
+        activity = request.GET.get("activity")
+        # find the schedule (if any) according to the id
+        targetSchedule = Schedule.objects.filter(id=id).first()
+        if targetSchedule:
+            # todo: add activity to Schedule.initiActs
 
 def deleteAct(request):
     return HttpResponse("Hello, world. You're at the schedule deleteAct.")
