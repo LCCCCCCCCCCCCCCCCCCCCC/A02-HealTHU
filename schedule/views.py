c68063ae03c38d0e3970b06280ee49527dd4b8bf
者霖 陈 <2824964418@qq.com>
2023-12-05 13:54:35 +0800

    migrations

--- schedule/views.py
+++ schedule/views.py
@@ -11,3 +11,17 @@ from .models import Activity
 import requests
 import json
 # Create your views here.
+def todos(request):
+    return HttpResponse("Hello, world. You're at the schedule todos.")
+
+def changeTodo(request):
+    return HttpResponse("Hello, world. You're at the schedule changeTodo.")
+
+def addAct(request):
+    return HttpResponse("Hello, world. You're at the schedule addAct.")
+
+def findAct(request):
+    return HttpResponse("Hello, world. You're at the schedule findAct.")
+
+def partAct(request):
+    return HttpResponse("Hello, world. You're at the schedule partAct.")
