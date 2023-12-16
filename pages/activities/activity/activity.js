ad33cc09a3bbe1ee3ab356daee39e1e40e6dedb3
LC <lichang21@mails.tsinghua.edu.cn>
2023-12-17 02:27:16 +0800

    活动页面的具体元素

--- pages/activities/activity/activity.js
+++ pages/activities/activity/activity.js
@@ -5,7 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    title: "软件学院2023秋第10周集体锻炼",
+    promoter:"NLno", 
+    promoterId:3,
+    promoterUrl:"",
+    participantNum:2,
+    patticipants:["teto","GUMI"],
+    participantsId:[5,7],
+    partNumMin:2,
+    partNumMax:4,
+    date:"2023/12/18",
+    start:"17:00",
+    end:"18:00",
+    label:"打卡统计",
+    detail:"blablablabla",
+    images:[],
+    tags:["紫荆操场","飞盘","集体锻炼"],
+    state:0
   },
 
   /**
