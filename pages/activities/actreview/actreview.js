d954fabe7a5e83a6a16df353a481e323db9595b3
LC <lichang21@mails.tsinghua.edu.cn>
2023-12-20 22:13:07 +0800

    合并

--- pages/activities/actreview/actreview.js
+++ pages/activities/actreview/actreview.js
@@ -11,21 +11,39 @@ Page({
       end:"18:00",
     },
     reviewList: [
-      {id: 1, nickname:"GUMI", text:"软院 2022012222 张三", state:1},
-      {id: 2, nickname:"teto",  text:"软件学院 2021011111 李四，请让我参加活动，我什么都会做的！", state: 0},
-      {id: 404, nickname:"anonymous",  text:"让我看看", state: 2}
+      {id: 1, nickName:"GUMI", text:"软院 2022012222 张三"},
+      {id: 2, nickName:"teto",  text:"软件学院 2021011111 李四，请让我参加活动，我什么都会做的！"},
+      {id: 404, nickName:"anonymous",  text:"让我看看"}
     ],
     // 0未处理，1接受，2拒绝
     tempItem: {id: 1, nickname:"GUMI", text:"软院 2022012222 张三", state:1},
     reviewshow: false,
-    radio: "1"
+    radio: "1",
+    actId:""
   },
 
   reviewHandle() {
     if(this.data.radio === "1") {
       // 同意请求,操作对象为tempItem,更新reviewList状态
-
-      wx.showToast({ title: '已同意', icon: 'success' });
+      var id = wx.getStorageSync('id')
+      var applicationId = this.data.tempItem.id
+      console.log(this.data.tempItem)
+      var that = this
+      console.log(id + " " + applicationId)
+      wx.request({
+        url:'http://127.0.0.1:8000/schedule/appReply/',
+        header:{ 'content-type': 'application/x-www-form-urlencoded'},
+        data:{
+          id:id,
+          applicationId:applicationId,
+          isAgree: 1
+        },
+        method:'POST',
+        success:function(res){
+          wx.showToast({ title: '操作成功', icon: 'success' });
+          that.onLoad()
+        }
+      })
     }
     else if(this.data.radio === "2"){
       //拒绝请求
@@ -54,7 +72,61 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-    var actid = options.actid;
+    if(options.actid){
+      var actid = options.actid;
+      this.setData({actId:actid})
+    }
+    var id = wx.getStorageSync('id')
+    var that = this
+    wx.request({
+      url:'http://127.0.0.1:8000/schedule/getActDetail/',
+      data:{
+        actId: that.data.actId
+      },
+      method:'GET',
+      success:function(res){
+        var data = res.data
+        data.participantNum = data.participants.length
+        that.setData({
+          activity:data
+        })
+      }
+    })
+    wx.request({
+      url:'http://127.0.0.1:8000/schedule/getApplication/',
+      data:{
+        id: id
+      },
+      method:'GET',
+      success:function(res){
+        var data = res.data
+        var filteredApps = data.filter(function(data) {
+          return actid == data.actId
+        });
+        console.log(filteredApps)
+        for(let i = 0;i< filteredApps.length;i++){
+          var j = 0
+          filteredApps[i].text = filteredApps[i].message
+          //filteredApps[i].id = filteredApps[i].applyerId
+          wx.request({
+            url:'http://127.0.0.1:8000/user/getDetail/',
+            data:{
+              'hostId': id,
+              'customerId':filteredApps[i].applyerId
+            },
+            method:'GET',
+            success:function(res){
+              var dataa = JSON.parse(res.data)
+              filteredApps[i].nickName = dataa.nickName
+              if(j == filteredApps.length - 1){
+                that.setData({reviewList:filteredApps})
+              }
+              j++
+            }
+          })
+        }
+      }
+    })
     // TODO：请求获取申请信息（需要对重复提交的进行筛选吗？），获取活动人数限制相关信息到activity
     // 但不知道是否要保留已经处理过的请求
   },
