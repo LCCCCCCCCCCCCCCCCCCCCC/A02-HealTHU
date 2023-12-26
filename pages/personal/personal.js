a0c237bea44a1e0e4ae3a8601aba9349dd67f585
LC <lichang21@mails.tsinghua.edu.cn>
2023-12-27 05:40:44 +0800

    小修改

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,4 +1,3 @@
-const { ParticleDefaultComponents } = require("XrFrame/elements");
 
 // pages/personal/personal.js
 Page({
@@ -44,6 +43,7 @@ Page({
     } 
     var id = wx.getStorageSync('id')
     var that = this
+    /*
     if(1 == 0){
       wx.request({
         url:'http://127.0.0.1:8000/user/getPersonal/',
@@ -77,6 +77,7 @@ Page({
         }
       })
     }
+    */
     var id = wx.getStorageSync('id')
     this.setData({
       id:id
