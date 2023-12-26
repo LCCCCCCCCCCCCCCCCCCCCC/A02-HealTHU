d3114b718ed08cc4838fc80c11adc88d531e2e60
LC <lichang21@mails.tsinghua.edu.cn>
2023-12-27 00:58:44 +0800

    主页相关

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -2,12 +2,11 @@
 Page({
   data: {
     active: 0,
-    userInfo: {
-      avatarUrl: '',
-      nickName: '微信用户',
-      id:1,
-      sign:"这个人很懒，即使这样也没有留下签名"
-    },
+    nickName: '微信用户',
+    avatarUrl: '',
+    signature: "这个人很懒，即使这样也没有留下签名",
+    followed: false,
+    userId: 0,
     id:1, // 用于确定是否为自己主页
     // 展示最近获得的6个成就
     achList: [
@@ -19,15 +18,15 @@ Page({
       {img: "../images/ach7.png",text: "月度纪念2023", time: "2023/7", detail:"当月打卡日期超过10天", state: 100},
     ],
     bbsList: [
-      {name:"NLno", title:"今天真冷啊..", time:"2023-12-18 13:56", url: '../bbs/bbs?bbsid=103'},
-      {name:"NLno", title:"[提问氵]西操体育馆几点开放啊", time:"2023-12-17 18:56", url: '../bbs/bbs?bbsid=102'},
-      {name:"NLno", title:"[失物招领]在紫操西北角捡到一串钥匙，已经交到紫荆一楼了", time:"2023-12-17 17:30", url: '../bbs/bbs?bbsid=101'},
+      { title:"今天真冷啊..", time:"2023-12-18 13:56", id: 103},
+      {title:"[提问氵]西操体育馆几点开放啊", time:"2023-12-17 18:56", id:102},
+      {title:"[失物招领]在紫操西北角捡到一串钥匙，已经交到紫荆一楼了", time:"2023-12-17 17:30", id:101},
       // {name:"NLno", title:"[新成就] “一年的坚持与守望”", time:"2023-11-31 10:02", url: '../bbs/bbs?bbsid=100'}
     ],
     actList: [
     ],
     actaddList: [
-      {name:"NLno", title:"[活动报名] 2023秋软件学院集体锻炼", time:"2023-12-18 9:20", url: '../activities/activity/activity?actid=10001'},
+      {name:"NLno", title:"[活动报名] 2023秋软件学院集体锻炼", time:"2023-12-18 9:20", id: 10001},
     ]
   },
   onChange(event) {
@@ -39,18 +38,13 @@ Page({
   onLoad(options) {
     if (options.id) {
       // 获取options.id对应的用户信息
-
-      var userInfo = this.data.userInfo;
-      userInfo.id = options.id;
-      this.setData({ userInfo: userInfo });
+      this.setData({ userId: options.id });
     } 
 
-    wx.getStorage({
-      key: 'id',
-      success: function(res) {
-        this.setData({'id': res.data});
-      }
-    });
+    var id = wx.getStorageSync('id')
+    this.setData({
+      id:id
+    })
   },
 
   /**
