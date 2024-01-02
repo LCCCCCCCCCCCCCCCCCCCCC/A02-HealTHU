54ed5e5ebb3066ca25b08522aaaa9996d1fcb30c
者霖 陈 <2824964418@qq.com>
2024-01-02 18:21:45 +0800

    Update sleep complete ?

---
+++ backend/sleep/views.py
@@ -0,0 +1,66 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.core import serializers
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import SleepInfo
+from datetime import datetime
+import requests
+import json
+
+
+# Create your views here.
+@csrf_exempt
+def changeSleep(request):
+    if request.method == 'POST':
+        id = request.POST.get('id')
+        date = request.POST.get('date')
+        data = request.POST.get('data')
+        # find the corresponding user, if not exist, create one
+        targetSleepInfo = SleepInfo.objects.filter(userId=id)
+        if targetSleepInfo is None:
+            targetSleepInfo = SleepInfo.objects.create(userId=id, sleepingInfo={})
+        targetSleepInfo.sleepingInfo[date] = data
+        targetSleepInfo.save()
+        return HttpResponse("Change Success")
+    
+@csrf_exempt
+def changeSleepState(request):
+    if request.method == 'POST':
+        # !!! no need to implement this function
+        return HttpResponse("Change Success")
+    
+def getSleep(request):
+    if request.method == 'GET':
+        id = request.GET.get('id')
+        date = request.GET.get('date')
+        # for this day and the 7 days before,
+        targetSleepInfo = SleepInfo.objects.filter(userId=id)
+        if targetSleepInfo is None:
+            targetSleepInfo = SleepInfo.objects.create(userId=id, sleepingInfo={})
+        timeDelta = 0
+        sleepInfo = {}
+        while timeDelta >= -7:
+            # date being a string, get thisDate as a new string that this date += timeDelta
+            thisDate = datetime.strptime(date, '%Y-%m-%d') + datetime.timedelta(days=timeDelta)
+            # if the date is not in the database, put a date, [0,0,0,0,0,0,0,0,0,0,0,0] into the dict
+            if targetSleepInfo.sleepingInfo.get(thisDate) is None:
+                sleepInfo[thisDate] = [0,0,0,0,0,0,0,0,0,0,0,0]
+            else:
+                sleepInfo[thisDate] = targetSleepInfo.sleepingInfo.get(thisDate)
+            timeDelta -= 1
+        return HttpResponse(json.dumps(sleepInfo, ensure_ascii=False))
+            
+        
+        
+        
+        
+
+
+
+
+
+
+
+
